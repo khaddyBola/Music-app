df59800ff39505f1ceb0d235754a87fcb726da73
khaddyBola <khadijahmusa232@gmail.com>
2022-08-27 13:53:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Player from "./components/Player";
+
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Bandana",
+      artist: "Fireboy DML & Asake",
+      img_src: "./images/Bandana.jpg",
+      src: "./songs/Fireboy_DML_ft_Asake_Bandanna.mp3"
+    },
+    {
+      title: "Terminator",
+      artist: "Asake",
+      img_src: "./images/Terminator.jpg",
+      src: "./songs/Asake_-_Terminator.mp3"
+    },
+    {
+      title: "Emi lo kan",
+      artist: "Qdot",
+      img_src: "./images/qdot.jpg",
+      src: "./songs/Emi-Lo-Kan.mp3"
+    },
+    {
+      title: "Buga (Lo Lo Lo)",
+      artist: "Kizz-Daniel, TEkno",
+      img_src: "./images/Buga.jpg",
+      src: "./songs/Tekno-ft-Kizz-Daniel-Buga.mp3"
+    },
+    {
+      title: "Feel-Good",
+      artist: "Mohbad",
+      img_src: "./images/Mohbad-Feel-Good.jpg",
+      src: "./songs/Mohbad-Feel-Good.mp3"
+    },
+    {
+      title: "Loving-You",
+      artist: "Zinoleesky",
+      img_src: "./images/Zinoleesky-Loving.jpg",
+      src: "./songs/Zinoleesky-Loving-You.mp3"
+    },
+    {
+      title: "Peace-be-upon-you-PBUY",
+      artist: "Asake",
+      img_src: "./images/Asake-PBUY.jpg",
+      src: "./songs/Asake-PBUY.mp3"
+    },
+  ])
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0
+      } else {
+        return currentSongIndex + 1
+      }
+    })
+  }, [currentSongIndex, songs.length])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player 
+        currentSongIndex = {currentSongIndex}
+        setCurrentSongIndex = {setCurrentSongIndex}
+        nextSongIndex = {nextSongIndex}
+        songs = {songs}
+      />
+     
     </div>
   );
 }
